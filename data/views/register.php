6603b05c1d4be6103914362c53af211167c3e4c6
Nokrad997 <zajkowskizajko@gmail.com>
2023-10-31 15:06:16 +0100

    Landing page with css

---
+++ data/views/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+        <title>WDPaI Project</title>
+        <link rel="stylesheet" type="text/css" href="../css/mainPage.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;1,400&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="gradient">
+            <div class="banner">
+                <h1> chuj </h1>
+            </div>
+        
+            <div class="login"> 
+                <form action="login.php" method="post">
+                    <input id="loginInput" type="text" name="login" placeholder="Login" onfocus="this.placeholder=''" onblur="this.placeholder='Nickname'"></br>
+                    <input id="emailInput" type="email" name="email" placeholder="Email" onfocus="this.placeholder=''" onblur="this.placeholder='Email'"></br>
+                    <input id="passwordInput" type="password" name="password" placeholder="Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'"></br>
+                    <input id="rePasswordInput" type="password" name="Re-enter" placeholder="Re-enter password" onfocus="this.placeholder=''" onblur="this.placeholder='Re-enter password'"></br>
+                    <input id="registerButton" type="submit" value="Register">
+
+                    <script 
+                </form>
+            </div>
+    </div>
+    </body>
+</html>
